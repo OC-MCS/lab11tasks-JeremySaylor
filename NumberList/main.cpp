2d29b48bb352e813bcf9aab42ec5885c282de50b
Jeremy Saylor <Jeremy.saylor@eagles.edu.oc>
2019-04-01 00:18:25 -0500

    update

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "NumberList.h"
+#include "NumberList.cpp"
+
 using namespace std;
 
 int main()
 {
-	 
+	NumberList Jeremy;
+	Jeremy.insertNode(10);
+	Jeremy.appendNode(40);
+	Jeremy.insertNode(30);
+	Jeremy.insertNode(25);
+	Jeremy.deleteNode(30);
+	Jeremy.displayList();
+
+
+
 	return 0;
 }
